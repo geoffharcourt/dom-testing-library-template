b7976fb8982498917d4650af0bc1b8cb89f065e4
Geoff Harcourt <geoff.harcourt@gmail.com>
2020-06-14 16:11:03 -0400

    Failing test

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,11 +3,10 @@ import {render, fireEvent} from '@testing-library/react'
 import Counter from '../'
 
 test('increments the count', () => {
-  const {getByText} = render(<Counter />)
-  const button = getByText('0')
+  const {getByTestId} = render(<Counter />)
+  const button = getByTestId('my-test-id')
   fireEvent.click(button)
   expect(button).toHaveTextContent('1')
   fireEvent.click(button)
   expect(button).toHaveTextContent('2')
 })
-
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Counter() {
   const [count, setCount] = React.useState(0)
   const increment = () => setCount(c => c + 1)
-  return <button onClick={increment}>{count}</button>
+  return <button data-test="my-test-name" onClick={increment}>{count}</button>
 }
 
 export default Counter
